e76e69f89f64d76bad165e5038540ee07bec1b6e
saeed <s.balkhair@hotmail.com>
2020-10-22 09:33:03 +0300

    Initial check-in of module lesson8

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            #region Example 1
+            //Console.WriteLine("Enter your name");
+            //var name = Console.ReadLine();
+            //var nameCharArray = name.ToCharArray();
+
+            //Array.Reverse(nameCharArray);
+            //foreach (var letter in nameCharArray)
+            //{
+            //    Console.Write(letter);
+            //}
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Example 2
+            //var counter = 0;
+            //List<int> numbers = new List<int>();
+            //while (counter < 5)
+            //{
+            //    Console.WriteLine("Enter number");
+            //    var number = Convert.ToInt32(Console.ReadLine());
+            //    if (numbers.Contains(number))
+            //    {
+            //        Console.WriteLine("Try enter another number");
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        numbers.Add(number);
+            //        counter++;
+            //    }
+            //}
+
+            //numbers.Sort();
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Example 3
+
+
+            //List<int> numbers = new List<int>();
+            //while (true)
+            //{
+            //    Console.WriteLine("Enter number or quit to exit");
+            //    var input = Console.ReadLine();
+            //    if (input == "quit")
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        var number = Convert.ToInt32(input);
+            //        if (!numbers.Contains(number))
+            //        {
+            //            numbers.Add(number);
+            //        }
+            //    }
+
+
+            //}
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Console.ReadKey();
+            #endregion
+
+        }
+    }
+}
